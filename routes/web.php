e2c93ca65bc7752289279d6d023c83dc1f77fa48
wayn3r <w9delacruz@gmail.com>
2022-02-12 15:13:19 -0400

    set home page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,24 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return new JsonResponse(
+        [
+            'routes' => [
+                'breed' => [
+                    'Create' => 'POST /api/breed',
+                    'List all' => 'GET /api/breed',
+                    'Search by name' => 'GET /api/breed?q=test',
+                    'Update' => 'PUT /api/breed',
+                    'Delete' => 'DELETE /api/breed',
+                ],
+                'cat' => [
+                    'Create' => 'POST /api/cat',
+                    'List all' => 'GET /api/cat',
+                    'Search by id' => 'GET /api/cat?id=1',
+                    'Update' => 'PUT /api/cat',
+                    'Delete' => 'DELETE /api/cat',
+                ],
+            ]
+        ]
+    );
 });
